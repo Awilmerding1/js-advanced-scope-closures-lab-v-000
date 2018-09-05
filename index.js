9ed39dfc7331c79b747bc09ee12ab50b3c5da11d
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-09-05 17:58:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ function produceDrivingRange(blockRange) {
   return function(start, end){
     let a = parseInt(start) 
     let b = parseInt(end)
-    let trip;
     if (a > b) {
       let trip = a - b
     } else {
