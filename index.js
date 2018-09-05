43abeb99ed1b96f977254ec00e63bc103fc1a546
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-09-05 17:36:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function produceDrivingRange(blockRange) {
   return function(start, end){
-    if (start-end > blockRange) {
+    if ((start-end) > blockRange) {
       return `$ blocks out of range`
     }
   }
