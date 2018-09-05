4cc69fe52e5dcb9a0bdeef5c0ccde9c57e55bfa9
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-09-05 18:11:33 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,38 @@
+function produceDrivingRange(blockRange) {
+  return function(start, end){
+    let a = parseInt(start) 
+    let b = parseInt(end)
+    let trip;
+    if (a < b) {
+       trip = b - a
+    } else {
+       trip = a - b
+    }
+    if (trip > blockRange) {
+      return `${trip-blockRange} blocks out of range`
+    } else {
+      return `within range by ${blockRange - trip}`
+    }
+  }
+}
+
+function produceTipCalculator(fare) {
+  return function(percent) {
+    return fare*percent
+  }
+}
+  
+function createDriver() {
+	let DriverId = 0;
+  return class  {
+    constructor(name) {
+      this.id = ++ DriverId
+      this.name = name
+    }
+  }
+}
+
+const Driver = createDriver()
+
+
+
