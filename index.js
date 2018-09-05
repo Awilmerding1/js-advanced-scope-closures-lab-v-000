4707e9fd21e6200b2fb14817c154b7789f8d6aca
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-09-05 17:49:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 function produceDrivingRange(blockRange) {
   return function(start, end){
     let a = Number.parseInt(start, 10) 
-   let b = Number.parseInt(end, 10)
-    if (start > end) {
-      let trip = start - end
+    let b = Number.parseInt(end, 10)
+    if (a > b) {
+      let trip = a - b
     } else {
-      let trip = end - start
+      let trip = b - a
     }
     if (trip > blockRange) {
       return `${trip} blocks out of range`
