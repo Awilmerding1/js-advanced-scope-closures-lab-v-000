51cc83c377a223920e09ca567e9ecc7dc0bd2a9c
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-09-05 17:43:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ function produceDrivingRange(blockRange) {
   return function(start, end){
     if (start > end) {
       let trip = start - end
+    } else {
+      let trip = end - start
     }
     let trip = start - end
     if (trip> blockRange) {
